04ea53fc021f004654b2f0a0dd9636fcb7b4f43c
Drevviann <drevviann@gmail.com>
2024-09-06 15:35:54 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,78 @@
+let questions = [
+    {
+        numb: 1,
+        question:
+            "Huruf Apa Ini? あ",
+        answer: "A",
+        options: [
+            "E",
+            "Ne",
+            "A",
+            "Fu"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Huruf Apa Ini? し",
+        answer: "Shi",
+        options: [
+            "Shi",
+            "Ne",
+            "Fu",
+            "Ta"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Huruf Apa Ini? れ",
+        answer: "Re",
+        options: [
+            "Nu",
+            "E",
+            "Ke",
+            "Re"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Huruf Apa Ini? ね",
+        answer: "Ne",
+        options: ["Ne", "I", "Wo", "Fu"] 
+    },
+    {
+        numb: 5,
+        question: "Huruf Apa Ini? ち",
+        answer: "Chi",
+        options: ["Wo", "Nn", "Chi", "Ra"]
+    },
+    {
+        numb: 6,
+        question: "Huruf Apa Ini? ふ",
+        answer: "Fu",
+        options: ["Fu", "Ta", "Ne", "No"]
+    },
+    {
+        numb: 7,
+        question: "Huruf Apa Ini? め",
+        answer: "Me",
+        options: ["Sa", "So", "Na", "Me"]
+    },
+    {
+        numb: 8,
+        question: "Huruf Apa Ini? ぬ",
+        answer: "Nu",
+        options: ["O", "E", "Ka", "Nu"]
+    },
+    {
+        numb: 9,
+        question: "Huruf Apa Ini? の",
+        answer: "No",
+        options: ["Ri", "Nn", "Tsu", "No"]
+    },
+    {
+        numb: 10,
+        question: "Huruf Apa Ini? え",
+        answer: "E",
+        options: ["Ta", "Ke", "E", "Nn"]
+    }
+];
